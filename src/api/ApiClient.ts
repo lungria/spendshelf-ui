808257984c2992f472b33b8bc04003d7607e37c1
Artem <kolomytsev1996@gmail.com>
2020-01-10 01:31:10 +0200

    rm mock

--- src/api/ApiClient.ts
+++ src/api/ApiClient.ts
@@ -16,36 +16,6 @@ interface ApiClient {
   SendTransaction(request: SendTransactionRequest): Promise<void>;
 }
 
-let api: ApiClient
-
-/* if (process.env.USE_MOCK === 'true') {
-  class MockApiClient implements ApiClient {
-    async GetCategories (): Promise<CategoriesResponse> {
-      return new Promise<CategoriesResponse>((resolve) => {
-        resolve({
-          categories: [
-            new Category('test1', 'id1'),
-            new Category('test2', 'id2')
-          ] })
-      })
-    }
-
-    async SendTransaction (request: SendTransactionRequest): Promise<void> {
-      return new Promise((resolve) => {
-        console.log(request)
-        resolve()
-      })
-    }
-
-    async CreateCategory (name: string): Promise<Category> {
-      return new Promise<Category>((resolve) => {
-        resolve(new Category(name, Math.random().toString(36)))
-      })
-    }
-  }
-
-  api = new MockApiClient()
-} else { */
 class SpendshelfApiClient implements ApiClient {
   async GetCategories (): Promise<Map<string, Category>> {
     let response = await fetch(serverUrl + '/categories')
@@ -88,6 +58,6 @@ class SpendshelfApiClient implements ApiClient {
   }
 }
 
-api = new SpendshelfApiClient()
+let api = new SpendshelfApiClient()
 
 export default api as ApiClient
